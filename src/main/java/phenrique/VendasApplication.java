be20c341a89b6ba410881c1860b60eba7e586ca0
paullo-hh <contacthenry96@gmail.com>
2024-01-29 12:02:02 -0300

    Ajustado e inserido novas annotations

--- src/main/java/phenrique/VendasApplication.java
+++ src/main/java/phenrique/VendasApplication.java
@@ -1,10 +1,9 @@
 package phenrique;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,23 +19,13 @@ public class VendasApplication {
     @Value("${application.name}")
     private String applicationName;
 
-    @Cachorro
-    private Animal animal;
-
-    @Bean (name = "executarAnimal")
-    public CommandLineRunner executar () {
-        return args -> {
-            this.animal.fazerBarulho();
-        };
-    }
-
     @GetMapping("/hello")
     public String helloPapai() {
         return applicationName;
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(VendasApplication.class, args);
+    public static void main(String[] args, ConfigurableApplicationContext ConfigurableApplicationContext) {
+        ConfigurableApplicationContext = SpringApplication.run(VendasApplication.class, args);
     }
 
 }
